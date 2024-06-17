6b2f8905a1572e412c135de29deb789f7cdb804a
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-06-17 22:58:58 +0300

    Add function getComputerChoice

--- myscript.js
+++ myscript.js
@@ -1,1 +1,12 @@
-console.log("hi");
+// Computer logic - should return choise
+
+function getComputerChoice () {
+    let random = Math.floor(Math.random() * 100);
+    let choise = (random < 33) ? "scissor" : 
+        (random < 66) ? "rock" :
+        "paper";
+
+    return choise
+}
+
+console.log(getComputerChoice())
