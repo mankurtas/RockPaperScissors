89f687bed916f7030e4e6cee2601483f44b6ed42
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-06-17 23:40:44 +0300

    Add one round logic

--- myscript.js
+++ myscript.js
@@ -15,3 +15,25 @@ function getHumanChoice() {
     let choice = prompt("Enter your choice - Rock/Paper/Scisser");
     return choice.toLowerCase();
 }
+
+const humanScore = 0;
+const computerScore = 0;
+
+//Logic to play one round
+
+function playRound (humanChoice, computerChoice) {
+    let winner = (humanChoice === "rock" 
+    && computerChoice ==="scissor") ? "You won!" :
+    (humanChoice === "scissor" && computerChoice === "paper") ? "You won!" :
+    (humanChoice === "paper" && computerChoice === "rock") ? "You won!" :
+    (humanChoice === computerChoice) ? "It is a tie" :
+    "You losed";
+
+    return winner + '\nHuman selection ' + humanChoice + '\nComputer selection ' + computerChoice
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+
+console.log(playRound(humanSelection, computerSelection))
