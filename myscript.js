98cbd8283e7c9bbe6477c8e6c65631db1b8bd1db
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-06-18 22:02:31 +0300

    Update play Round function to more readable if else add. return resuslt and add point to winner

--- myscript.js
+++ myscript.js
@@ -16,24 +16,50 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
-const humanScore = 0;
-const computerScore = 0;
+let humanScore = 0;
+let computerScore = 0;
 
 //Logic to play one round
 
 function playRound (humanChoice, computerChoice) {
-    let winner = (humanChoice === "rock" 
-    && computerChoice ==="scissor") ? "You won!" :
-    (humanChoice === "scissor" && computerChoice === "paper") ? "You won!" :
-    (humanChoice === "paper" && computerChoice === "rock") ? "You won!" :
-    (humanChoice === computerChoice) ? "It is a tie" :
-    "You losed";
 
-    return winner + '\nHuman selection ' + humanChoice + '\nComputer selection ' + computerChoice
+    let result;
+    if (
+        (humanChoice === "rock" && computerChoice ==="scissor") || 
+        (humanChoice === "scissor" && computerChoice === "paper") ||
+        (humanChoice === "paper" && computerChoice === "rock")
+    ) {
+        result = "You won!"; 
+        humanScore++;
+    } else if (humanChoice === computerChoice) {
+        result = " It is a tie";
+    } else {
+        result = "You lost";
+        computerScore++;
+    }
+
+    // return winner
+
+    // let winner = (humanChoice === "rock" 
+    // && computerChoice ==="scissor") ? humanScore+=humanScore :
+    // (humanChoice === "scissor" && computerChoice === "paper") ? "You won!" :
+    // (humanChoice === "paper" && computerChoice === "rock") ? "You won!" :
+    // (humanChoice === computerChoice) ? "It is a tie" :
+    // "You losed";
+
+    return result + '\nHuman selection ' + humanScore + " " + humanChoice + 
+    '\nComputer selection ' + computerScore + " " + computerChoice
+
+    // return result
 }
 
 const humanSelection = getHumanChoice();
 const computerSelection = getComputerChoice();
 
-
 console.log(playRound(humanSelection, computerSelection))
+
+// Logic to play entire game - 5 rounds
+
+// for (let i=0; i < 5; i++){
+
+// }
