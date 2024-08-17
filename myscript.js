1348d2258de0584422a92d732a93fbfbf74c51dd
mankurtas <71133480+mankurtas@users.noreply.github.com>
2024-08-17 13:17:25 +0300

    Add styling, images

--- myscript.js
+++ myscript.js
@@ -3,13 +3,24 @@ let computerScore = 0;
 
 let roundNo = 1;
 
-const plyaBtn = document.querySelector('.play')
+// Score-boerd items
+const hmScore = document.querySelector('.humanScores')
+const compScore = document.querySelector('.computerScores')
+
+
+const playBtn = document.querySelector('.play')
+
+// Play round sections items
+
+// const roundInfo = document.querySelector('.round-score')
+const round = document.querySelector('.round')
+
+
+// Choses options items
 const selectionsButtons = document.querySelectorAll('button.selection');
 
-const hmScore = document.querySelector('.humanScore')
-const compScore = document.querySelector('.computerScore')
-
-const round = document.querySelector('.round')
+// Game over section
+const noteToSelectOption = document.querySelector('.noteSelect')
 
 
 // Computer logic - should return choise
@@ -30,7 +41,8 @@ function getHumanChoice () {
 
         selectionsButtons.forEach(btn => {
             btn.addEventListener('click', (event) => {
-                const humanChoice = event.target.textContent;
+                // const humanChoice = event.target.textContent;
+                const humanChoice = event.target.alt
                 console.log(humanChoice);
 
                 // Resolve the promise with the user's choice
@@ -45,10 +57,13 @@ function getHumanChoice () {
 
 //Logic to play one round
 
-plyaBtn.addEventListener('click', playRound)
+playBtn.addEventListener('click', playRound)
 
 async function playRound (){
     round.textContent = roundNo;
+    // noteToSelectOption.textContent = "Choose your option:";
+
+    // roundInfo.textContent = ''
     roundNo++;
 
     const humanChoice = await getHumanChoice()
@@ -61,25 +76,46 @@ async function playRound (){
         (humanChoice === "Scissor" && computerChoice === "Paper") ||
         (humanChoice === "Paper" && computerChoice === "Rock")
     ) {
-        result = "You won!"; 
+        result = "You won the round!"; 
         humanScore++;
         hmScore.textContent = humanScore
 
     } else if (humanChoice === computerChoice) {
         result = "It is a tie";
     } else {
-        result = "You lost";
+        result = "You lost the round";
         computerScore++;
         compScore.textContent = computerScore
     }
+
+
+    // roundInfo.innerHTML = "Your selection: " + humanChoice + " vs Computer selection: " + computerChoice + '<br>' + result
 
     console.log(computerChoice  + ' comp')
     console.log(humanChoice + ' human')
     console.log(result)
 
+    validateFivePointsWiner()
+
 }
 
+function validateFivePointsWiner () {
+    if (humanScore == 5 || computerScore ==5) {
+        
+        alert('Game is over human score ' + humanScore + ' COM SCORE ' + computerScore)
+        
+        humanScore = 0;
+        hmScore.textContent = "0"
 
+        computerScore = 0; 
+        compScore.textContent = '0';
+
+        
+        round.textContent = '0';
+        roundNo = 1;
+    }
+        
+}
 
 
 
